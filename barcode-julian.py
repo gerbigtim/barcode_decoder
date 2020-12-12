ca85de19b55542b734f3f6b6cd39ff773b3286a6
Tim Gerbig <tim.gerbig@gmail.com>
2020-12-12 15:24:41 +0100

    Make decoder class

--- barcode-julian.py
+++ barcode-julian.py
@@ -3,39 +3,35 @@ import pandas as pd
 import string
 
 
-def split_barcode(barcode):
-    """Split the barcode into a dictionary."""
-    barcode_dict = {}
-    barcode_dict['Marke'] = barcode[:2]
-    barcode_dict['Art'] = barcode[2:4]
-    barcode_dict['Material'] = barcode[4:6]
-    barcode_dict['Groesse'] = barcode[6:8]
-    return barcode_dict
+class Decoder():
+    """Decode and encode barcodes with csv."""
 
+    def Decoder(this, csv_file):
+        """Construct the class."""
+        this.df = pd.read_csv(csv_file)
 
-def decode_barcode(barcode, df):
-    """Decode the barcode."""
-    if not isinstance(barcode, str):
-        raise TypeError("Barcode must be passed as a string!")
-    if len(barcode) != 14:
-        raise ValueError("Barcode must be of length 14!")
-    if not all(c in string.hexdigits for c in barcode):
-        raise ValueError('Barcode contains non hex digits!')
-    barcode = barcode.lower()
+    def _split_barcode(this, barcode):
+        """Split the barcode into a dictionary."""
+        barcode_dict = {}
+        barcode_dict['Marke'] = barcode[:2]
+        barcode_dict['Art'] = barcode[2:4]
+        barcode_dict['Material'] = barcode[4:6]
+        barcode_dict['Groesse'] = barcode[6:8]
+        return barcode_dict
 
-    barcode_dict = split_barcode(barcode)
-    item_dict = {}
-    for key, code in barcode_dict.items():
-        item_dict[key] = df.loc[df[key+'Code'] == code][key].values[0]
-    item_dict['Farbe'] = barcode[8:]
-    return item_dict
+    def decode_barcode(this, barcode):
+        """Decode the barcode."""
+        if not isinstance(barcode, str):
+            raise TypeError("Barcode must be passed as a string!")
+        if len(barcode) != 14:
+            raise ValueError("Barcode must be of length 14!")
+        if not all(c in string.hexdigits for c in barcode):
+            raise ValueError('Barcode contains non hex digits!')
+        barcode = barcode.lower()
 
-
-def main():
-    """Execute the main function."""
-    df = pd.read_csv('DecodeSheet.csv')
-    print(decode_barcode('00010203FFgFFF', df))
-
-
-if __name__ == '__main__':
-    main()
+        barcode_dict = this._split_barcode(barcode)
+        item_dict = {}
+        for key, code in barcode_dict.items():
+            item_dict[key] = this.df.loc[this.df[key+'Code'] == code][key].values[0]
+        item_dict['Farbe'] = barcode[8:]
+        return item_dict

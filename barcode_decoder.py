19b8f834a20e808f635b4703b4331fd32944c82f
Tim Gerbig <tim.gerbig@gmail.com>
2020-12-12 16:54:48 +0100

    Add encoding

--- barcode_decoder.py
+++ barcode_decoder.py
@@ -1,6 +1,7 @@
 """Module for reading barcodes."""
 import pandas as pd
 import string
+import collections
 
 
 class Decoder:
@@ -9,18 +10,20 @@ class Decoder:
     def __init__(this, csv_file):
         """Construct the class."""
         this.df = pd.read_csv(csv_file)
-        this.keyes = this._get_keyes()
+        this._color = 'Farbe'
+        this._keys = this._get_keys()
 
-    def _get_keyes(this):
+    def _get_keys(this):
         """Get all named column names."""
-        keyes = this.df.columns
-        keyes = [key for key in keyes if not key.startswith('Unnamed:')]
-        return keyes
+        keys = this.df.columns
+        keys = [key for key in keys if not key.startswith('Unnamed:')]
+        keys.append(this._color)
+        return keys
 
     def _split_barcode(this, barcode):
         """Split the barcode into a dictionary."""
         barcode_dict = {}
-        for key, index in zip(this.keyes, range(0, 8, 2)):
+        for key, index in zip(this._keys, range(0, 8, 2)):
             barcode_dict[key] = barcode[index:index+2]
         return barcode_dict
 
@@ -37,8 +40,22 @@ class Decoder:
         barcode_dict = this._split_barcode(barcode)
         item_dict = {}
         for key, code in barcode_dict.items():
-            codes_idx = this.df.columns.get_indexer([key]) + 1
-            mask = (this.df.iloc[:, codes_idx] == code).iloc[:, 0]
+            idx = this.df.columns.get_indexer([key]) + 1
+            mask = (this.df.iloc[:, idx] == code).iloc[:, 0]
             item_dict[key] = this.df.loc[mask][key].values[0]
-        item_dict['Farbe'] = barcode[8:]
+        item_dict[this._color] = barcode[8:]
         return item_dict
+
+    def encode(this, item_dict):
+        """Encode dictionary into barcode."""
+        if not collections.Counter(item_dict.keys()) == collections.Counter(this._keys):
+            raise ValueError("Dictionary contains wrong keys.")
+        if not all(c in string.hexdigits for c in item_dict[this._color]):
+            raise ValueError('Color contains non hex digits!')
+        barcode = ''
+        for key in this._keys[:-1]:
+            value = item_dict[key]
+            idx = this.df.columns.get_indexer([key]) + 1
+            barcode += this.df.loc[this.df[key] == value].iloc[:, idx].values[0][0]
+        barcode += item_dict[this._color].lower()
+        return barcode
